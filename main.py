d31a71d39f4a1e1377d741e470038ecbbb7977a0
tomaraakansha <akanksha.official1@gmail.com>
2024-01-25 12:35:08 -0600

    Update .gitignore and main.py, and update requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Query
 from llm_model import *
 from dotenv import load_dotenv
-import os
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -13,8 +13,13 @@ def answer_query(query: str = Query(..., title="User Query", max_length=50)):
     if not query:
         return {"answer": "Please provide a query"}
     else:
-        create_vector_db_from_text_file("Data/TechData.txt", os.getenv("PINECONE_API_KEY"))
-        top_K_docs = search_and_retrieve(query, api_key=os.getenv("PINECONE_API_KEY"), k = 4)
-    
-    return {"answer": "Final Result after search and retrieval"}
+        create_vector_db_from_text_file("Data/TechData.txt")
+        docs_page_content = search_and_retrieve(query)
+        try:
+            response = get_response_from_llm(query, docs_page_content)
+            return {"answer": response}
+        except Exception as e:
+            print(e)
+            raise HTTPException(status_code=500, detail="Error occurred while getting response from LLM model")
+        
         
