f5f558a5fa93f053ab9c31c8272e86dcdc3c0424
tomaraakansha <akanksha.official1@gmail.com>
2024-01-29 19:31:35 -0600

    Add POST endpoint for predicting intent

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, HTTPException, Query
 from llm_model import *
 from dotenv import load_dotenv
-from fastapi import HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 load_dotenv()
+
+
+class Item(BaseModel):
+    api_key: str
+    collection_name: Optional[str] = "Test"
+    user_query: str
 
 # Endpoint for user queries
 @app.get("/answer_query")
@@ -22,4 +29,19 @@ def answer_query(query: str = Query(..., title="User Query", max_length=50)):
             print(e)
             raise HTTPException(status_code=500, detail="Error occurred while getting response from LLM model")
         
-        
+@app.post("/predict")
+def predict_intent(item: Item):
+    if not item.user_query:
+        return {"answer": "Please provide a query"}
+    elif not item.api_key:
+        return {"answer": "Please provide a valid API key"}
+    else:
+        if(item.collection_name == None or item.collection_name == ""):
+            item.collection_name = "Test"
+        OPENAI_API_KEY = item.api_key
+        try:
+            top_three_responses = ['response1', 'response2', 'response3']
+            return {"intents": top_three_responses}
+        except Exception as e:
+            print(e)
+            raise HTTPException(status_code=500, detail="Error occurred while getting response from LLM model")
