5693239f1c0f1db926540795affd6c45a4166e57
tomaraakansha <akanksha.official1@gmail.com>
2024-02-04 19:24:29 -0600

    remove predict api endpoint and code refactor

--- llm_model.py
+++ llm_model.py
@@ -6,95 +6,101 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain.chains import LLMChain
 from langchain_openai.chat_models import ChatOpenAI
 import json
+from dotenv import load_dotenv
+import os
 
-INDEX_NAME = "langchain-index"
-# initialize pinecone index
-def init_pinecone(index_name=INDEX_NAME):
-    pc = Pinecone()
-    if index_name not in pc.list_indexes().names():
-        pc.create_index(
-            name=index_name,
-            #The OpenAI embedding model `text-embedding-ada-002' uses 1536 dimensions
-            dimension=1536,
-            metric="euclidean",
-            spec={'pod': {'environment': 'gcp-starter',
-                               'pod_type': 'starter',
-                               'pods': 1,
-                               'replicas': 1,
-                               'shards': 1}},
-        ) 
-    index = pc.Index(index_name)
-    return index
+load_dotenv()
+
+class llm_model_class:
+    INDEX_NAME = "langchain-index"
+    index = None
+    # initialize pinecone index
+    def __init__(self, index_name=INDEX_NAME):
+        PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+        pc = Pinecone(api_key=PINECONE_API_KEY)
+        if(index_name in pc.list_indexes().names()):
+            pc.delete_index(index_name)
+        if index_name not in pc.list_indexes().names():
+            pc.create_index(
+                name=index_name,
+                #The OpenAI embedding model `text-embedding-ada-002' uses 1536 dimensions
+                dimension=1536,
+                metric="euclidean",
+                spec={'pod': {'environment': 'gcp-starter',
+                                'pod_type': 'starter',
+                                'pods': 1,
+                                'replicas': 1,
+                                'shards': 1}},
+            ) 
+        index = pc.Index(index_name)
+        self.index = index
 
 
-# create vector db from text file and upload to pinecone
-def create_vector_db_from_text_file(file_path, index_name=INDEX_NAME):
-    loader = TextLoader(file_path)
-    documents = loader.load()
-    
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
-    docs = text_splitter.split_documents(documents)
-    docs = [str(doc) for doc in docs]
-    
-    embeddings = OpenAIEmbeddings()
-    embeds = embeddings.embed_documents(docs)
-
-    # Create a list of dictionaries where each dictionary has an 'id' and 'values'
-    vectors = [{"id": f'doc_{i}', "values": embed} for i, embed in enumerate(embeds)]
-
-    index = init_pinecone(index_name)
-    index.upsert(vectors=vectors)
-    # Save the mapping of doc ids and their corresponding text to a json file
-    mappings = {f'doc_{i}': embed  for i, embed in enumerate(docs)}
-    with open('data/mapping.json', 'w') as fp:
-        json.dump(mappings, fp)
-
-    return index
-
-# search and retrieve from vector db
-def search_and_retrieve(query, index_name=INDEX_NAME,max_len=3750):
-    #get vector representation of query
-    embeddings = OpenAIEmbeddings()
-    query_vector = embeddings.embed_query(query)
-    index = init_pinecone(index_name)
-    search_res = index.query(vector=query_vector, top_k=4, include_metadata=True)
-    cur_len = 0
-    contexts = []
-    mappings = json.load(open('data/mapping.json', 'r'))
-    for row in search_res['matches']:
-        text = mappings[row['id']]
-        cur_len += len(text) + 4
-        if cur_len < max_len:
-            contexts.append(text)
-        else:
-            cur_len -= len(text) + 4
-            if max_len - cur_len < 200:
-                break
-    return "\\\\n\\\\n###\\\\n\\\\n".join(contexts)
-
-# get response from LLM model based on query and docs
-def get_response_from_llm(query, docs_page_content):
-    llm = ChatOpenAI(
-    model_name='gpt-3.5-turbo',
-    temperature=0.0
-    )
-
-    prompt = PromptTemplate(
-        input_variables=["query", "docs"],   
-        template = """
-        You are a helpful assistant that uses the following pieces of context to answer the users question.
-        Answer the following question: {question}
-        by searching the following context:{docs}
+    # create vector db from text file and upload to pinecone
+    def create_vector_db_from_file(self, file_content, OPENAI_API_KEY, index_name=INDEX_NAME):
+        docs = file_content.decode('utf-8')
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+        docs = text_splitter.create_documents([docs])
+        docs = [str(doc) for doc in docs]
         
-        Answer the question based on the context provided. If the
-        question cannot be answered using the information provided answer
-        with "I don't know".
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        embeds = embeddings.embed_documents(docs)
 
-        Your answer should be detailed.
-        """
-    )
+        # Create a list of dictionaries where each dictionary has an 'id' and 'values'
+        vectors = [{"id": f'doc_{i}', "values": embed} for i, embed in enumerate(embeds)]
 
-    chain = LLMChain(llm = llm, prompt = prompt)
-    response = chain.generate(input_list=[{"question": query, "docs": docs_page_content}])
-    answer = response.generations[0][0].text
-    return answer
+        self.index.upsert(vectors=vectors)
+        # Save the mapping of doc ids and their corresponding text to a json file
+        mappings = {f'doc_{i}': embed  for i, embed in enumerate(docs)}
+        if not os.path.exists('data'):
+            os.makedirs('data')
+        with open('data/mapping.json', 'w') as fp:
+            json.dump(mappings, fp)
+
+    # search and retrieve from vector db
+    def search_and_retrieve(self, query,OPENAI_API_KEY, index_name=INDEX_NAME,max_len=3750):
+        #get vector representation of query
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        query_vector = embeddings.embed_query(query)
+        search_res = self.index.query(vector=query_vector, top_k=4, include_metadata=True)
+        cur_len = 0
+        contexts = []
+        mappings = json.load(open('data/mapping.json', 'r'))
+        for row in search_res['matches']:
+            text = mappings[row['id']]
+            cur_len += len(text) + 4
+            if cur_len < max_len:
+                contexts.append(text)
+            else:
+                cur_len -= len(text) + 4
+                if max_len - cur_len < 200:
+                    break
+        return "\\\\n\\\\n###\\\\n\\\\n".join(contexts)
+
+    # get response from LLM model based on query and docs
+    def get_response_from_llm(self, query, docs_page_content, OPENAI_API_KEY):
+        llm = ChatOpenAI(
+        model_name='gpt-3.5-turbo',
+        temperature=0.0,
+        openai_api_key=OPENAI_API_KEY
+        )
+
+        prompt = PromptTemplate(
+            input_variables=["query", "docs"],   
+            template = """
+            You are a helpful assistant that uses the following pieces of context to answer the users question.
+            Answer the following question: {question}
+            by searching the following context:{docs}
+            
+            Answer the question based on the context provided. If the
+            question cannot be answered using the information provided answer
+            with "I don't know".
+
+            Your answer should be detailed.
+            """
+        )
+
+        chain = LLMChain(llm = llm, prompt = prompt)
+        response = chain.generate(input_list=[{"question": query, "docs": docs_page_content}])
+        answer = response.generations[0][0].text
+        return answer
--- main.py
+++ main.py
@@ -1,47 +1,31 @@
-from fastapi import FastAPI, HTTPException, Query
-from llm_model import *
-from dotenv import load_dotenv
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI, HTTPException, Query, File, UploadFile
+from llm_model import llm_model_class
 
 app = FastAPI()
 
-load_dotenv()
-
-
-class Item(BaseModel):
-    api_key: str
-    collection_name: Optional[str] = "Test"
-    user_query: str
-
 # Endpoint for user queries
-@app.get("/answer_query")
-def answer_query(query: str = Query(..., title="User Query", max_length=50)):
+@app.post("/answer_query_from_text_file")
+async def answer_query_from_text_file(query: str = Query(..., title="User Query", max_length=50), file: UploadFile = File(...), api_key: str = Query(..., title="API Key")):
     if not query:
         return {"answer": "Please provide a query"}
+    elif not file or not file.filename.endswith('.txt'):
+        return {"answer": "Please upload a .txt file"}
+    elif api_key == "" or api_key == None:
+        return {"answer": "Please provide OPENAI API key"}
     else:
-        create_vector_db_from_text_file("Data/TechData.txt")
-        docs_page_content = search_and_retrieve(query)
+        file_contents = await file.read()
         try:
-            response = get_response_from_llm(query, docs_page_content)
-            return {"answer": response}
+            response = get_response_from_llm(query, file_contents, api_key)
+            return response
         except Exception as e:
-            print(e)
             raise HTTPException(status_code=500, detail="Error occurred while getting response from LLM model")
         
-@app.post("/predict")
-def predict_intent(item: Item):
-    if not item.user_query:
-        return {"answer": "Please provide a query"}
-    elif not item.api_key:
-        return {"answer": "Please provide a valid API key"}
-    else:
-        if(item.collection_name == None or item.collection_name == ""):
-            item.collection_name = "Test"
-        OPENAI_API_KEY = item.api_key
-        try:
-            top_three_responses = ['response1', 'response2', 'response3']
-            return {"intents": top_three_responses}
-        except Exception as e:
-            print(e)
-            raise HTTPException(status_code=500, detail="Error occurred while getting response from LLM model")
+def get_response_from_llm(query, file_contents, api_key):
+    OPENAI_API_KEY = api_key
+    llm_model = llm_model_class()
+    llm_model.create_vector_db_from_file(file_contents, OPENAI_API_KEY)
+    docs_page_content = llm_model.search_and_retrieve(query, OPENAI_API_KEY)
+    response = llm_model.get_response_from_llm(query, docs_page_content, OPENAI_API_KEY)
+    return {"answer": response}
+    
+        
